6501fba19d3be756f352fd231b95d2d128da8992
Ewelina Lasowy <ewelinalasowy@wp.pl>
2021-01-08 18:24:48 +0100

    added fontAwesome

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,8 +1,26 @@
 <template>
   <div class="container mx-auto m-64">
-    <h2 class="text-indigo-500 font-black text-5xl">
-      {{ this.participants }}
+    <h2 class="text-indigo-500 font-black text-5xl pb-4">
+      {{ currentRoom.name }}
     </h2>
+
+    <h4
+      v-for="participant in participants"
+      v-bind:key="participant.id"
+      class="text-center text-xl"
+    >
+      <div v-if="participant.id == currentRoom.creator.id">
+        <p>
+          <i class="fas fa-crown text-indigo-800"></i>
+          {{ participant.username }}
+        </p>
+      </div>
+      <div v-else>
+        <p>
+          {{ participant.username }}
+        </p>
+      </div>
+    </h4>
   </div>
 </template>
 
